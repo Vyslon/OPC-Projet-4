980bbaba58aed97b17ff3ab58a818f6b4af672cb
Vyslon <santoni.thomas26@hotmail.fr>
2018-04-23 11:43:05 +0200

    Added name, description, stores and url to a dictionnary for all products in page 1 of product from product made from vegetables

--- Main.py
+++ Main.py
@@ -1,5 +1,14 @@
 # -*- coding: UTF-8 -*-
 import requests
+import json
 
-r = requests.get("https://fr.openfoodfacts.org/categories.json")
-print("reponse : {}".format(r.text))
+url = "https://fr.openfoodfacts.org/categorie/aliments-et-boissons-a-base-de-vegetaux.json"
+r = requests.get(url).json()
+
+dictionnary = {"pbfabP1" : []}
+for i in range(20):
+    dictionnary["pbfabP1"].append({'nom' : r['products'][i]['product_name_fr']})
+    dictionnary["pbfabP1"].append({'description' : r['products'][i]['categories']})
+    dictionnary["pbfabP1"].append({'revendeur(s)' : r['products'][i]['stores']})
+    dictionnary["pbfabP1"].append({'lien openfoodfacts' : r['products'][i]['url']})
+print("pbfabP1 : \n{}".format(dictionnary["pbfabP1"]))
