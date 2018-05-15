bf52f82c0f287aef185a5b8f7b9dfcf97ea0bc3d
Vyslon <santoni.thomas26@hotmail.fr>
2018-05-15 14:55:06 +0200

    Adding SQL requests to create tables and indexes

--- test.py
+++ test.py
@@ -1,16 +1,4 @@
-# -*- coding: UTF-8 -*-
-import requests
-import json
 import records
-from collections import deque
-from functions import *
-
-url_categories = "https://fr.openfoodfacts.org/categories.json"
-r_categories = requests.get(url_categories).json()
-categorie_list_url = []
-categorie_list_name = []
-products = deque()
-nb_categories_produits = 20
 
 db_connection = records.Database('mysql+pymysql://root:123@localhost')
 db_connection.query("DROP DATABASE IF EXISTS PROJET5;")
@@ -32,30 +20,4 @@ db_connection.query("CREATE UNIQUE INDEX unique_name ON Products_categories(name
 db_connection.query("DROP TABLE IF EXISTS VM_Final;")
 db_connection.query("CREATE TABLE VM_Final ENGINE = InnoDB SELECT Products.name AS Nom_Produit, Products.nutrition_grade AS Note_Nutritionnelle, Products.description AS Description, Products.stores AS Revendeurs, Products_categories.name AS Nom_Catégorie FROM PC_C_association_table INNER JOIN Products_categories ON PC_C_association_table.cat_id = Products_categories.id INNER JOIN Products ON PC_C_association_table.product_id = Products.id ORDER BY Products.nutrition_grade;")
 
-for a in range(0, nb_categories_produits):
-    categorie_list_url.append(r_categories['tags'][a]['url'])
-
-for a in range(0, nb_categories_produits):
-    categorie_list_name.append(r_categories['tags'][a]['name'])
-
-for k in range(0, nb_categories_produits):
-    url_cat = categorie_list_url[k] + "/"
-    for j in range(1, 21):
-        final_url = url_cat + str(j) + ".json"
-        r_products = requests.get(final_url).json()
-        for i in range(0, nb_categories_produits):
-            try:
-                #print("url : {}\n".format(final_url))
-                #print("{}\n".format(i))
-                products.append({'nom' : r_products['products'][i]['product_name'],\
-                'description' : r_products['products'][i]['categories'],\
-                'revendeur(s)' : r_products['products'][i]['stores'],\
-                'lien openfoodfacts' : r_products['products'][i]['url'],\
-                'note nutritionnelle' : r_products['products'][i]['nutrition_grade_fr'],\
-                'cat0' : r_products['products'][i]['categories_hierarchy'][0],\
-                'cat1' : r_products['products'][i]['categories_hierarchy'][1],\
-                'cat2' : r_products['products'][i]['categories_hierarchy'][2],
-                })
-                #print(r_products['products'][i]['stores'])
-            except:
-                pass
+"{}".format(var) POUR REMPLIR LA BASE DE DONNEES
