fdbfbcd5667ebc53a6b8c06d57a16b68521bb9d7
Vyslon <santoni.thomas26@hotmail.fr>
2018-06-01 17:32:51 +0200

    Added a new feature

--- Substitute.py
+++ Substitute.py
@@ -28,10 +28,12 @@ while (restart == "0"):
         rows_final = db_connection.query("SELECT Nom_Produit, Note_Nutritionnelle, Revendeurs, URL_off FROM VM_Final WHERE Nom_Categorie IN (\"{}\", \"{}\", \"{}\") ORDER BY Note_Nutritionnelle;".format(categorie_list_name_S[(rc_list[0]-1)], categorie_list_name_S[(rc_list[1]-1)], categorie_list_name_S[(rc_list[2]-1)]))
         substitute = rows_final[0]
         print("Nom du produit substitutant = {}\nNote nutritionnelle du produit substituant = {}\nRevendeur du produit substituant = {}\nlien openfoodfacts du produit substituant = {}\n\n".format(substitute["Nom_Produit"], substitute["Note_Nutritionnelle"], substitute["Revendeurs"], substitute["URL_off"]))
-        rows_substitute = db_connection.query("SELECT id FROM Products WHERE name = \"{}\"".format(substitute["Nom_Produit"]))
-        id_substitute = rows_substitute.first()
-        id_substitute = id_substitute["id"]
-        db_connection.query("UPDATE Products SET substitute = true WHERE id = \"{}\"".format(id_substitute))
+        save = input("Voulez vous sauvegarder le produit substitutant ? 1 - Sauvegarder 2 - Ne pas sauvegarder\n")
+        if save == 1:
+            rows_substitute = db_connection.query("SELECT id FROM Products WHERE name = \"{}\"".format(substitute["Nom_Produit"]))
+            id_substitute = rows_substitute.first()
+            id_substitute = id_substitute["id"]
+            db_connection.query("UPDATE Products SET substitute = true WHERE id = \"{}\"".format(id_substitute))
         restart = input("0 - Retour menu\n1 - Fermer programme\n\n")
     elif nb_result == "2":
         db_connection = records.Database('mysql+pymysql://root:123@localhost/?charset=utf8mb4')
